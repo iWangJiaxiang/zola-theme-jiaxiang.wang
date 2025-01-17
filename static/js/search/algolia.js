6de5ff8d9f9e67ff34a40dead0cfc8168b7db60d
Wang Jiaxiang <to@jiaxiang.wang>
2025-01-18 04:01:08 +0800

    ✨ Support js and css loading on demand

--- static/js/search/algolia.js
+++ static/js/search/algolia.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
     const $searchMask = document.getElementById("search-mask");
     const $searchDialog = document.querySelector("#algolia-search .search-dialog");
-  
+
     const openSearch = () => {
       btf.animateIn($searchMask, "to_show 0.5s");
       $searchDialog.style.display = "block";
@@ -19,6 +19,13 @@ window.addEventListener("load", () => {
   
       fixSafariHeight();
       window.addEventListener("resize", fixSafariHeight);
+
+      const searchInput = document.getElementsByClassName("ais-SearchBox-input")[0];
+      searchInput.disabled = true;
+      loadFiles(searchFiles, () => {
+        initSearch();
+        //searchInput.disabled = false;
+      });
     };
 
     // shortcut: shift+S
@@ -65,169 +72,175 @@ window.addEventListener("load", () => {
       document.querySelector("#algolia-search .search-close-button").addEventListener("click", closeSearch);
     };
   
-    const searchConfig = GLOBAL_CONFIG.search;
-    var searchClient;
-    var indexName;
+    const initSearch = () => {
+      const searchConfig = GLOBAL_CONFIG.search;
+      var searchClient;
+      var indexName;
 
-    if(searchConfig.engine == 'algolia') {
-      const config = searchConfig.algolia;
-      const isAlgoliaValid = config.appId && config.apiKey && config.indexName;
-      if (!isAlgoliaValid) {
-        return console.error("Algolia Search setting is invalid!");
-      }
-      searchClient = algoliasearch(config.appId, config.apiKey);
-      indexName = config.indexName;
-    } else if (searchConfig.engine == 'meilisearch') {
-      const config = searchConfig.meilisearch;
-      const isAlgoliaValid = config.host && config.apiKey && config.indexName;
-      if (!isAlgoliaValid) {
-        return console.error("Algolia Search setting is invalid!");
-      }
-      searchClient = instantMeiliSearch(config.host, config.apiKey).searchClient;
-      indexName = config.indexName;
-    }
-  
-    const search = instantsearch({
-      indexName: indexName,
-      searchClient: searchClient,
-      searchFunction(helper) {
-        if (helper.state.query) {
-          let innerLoading = '<i class="icon-spinner spin"></i>';
-          document.getElementById("algolia-hits").innerHTML = innerLoading;
-          helper.search();
+      if(searchConfig.engine == 'algolia') {
+        const config = searchConfig.algolia;
+        const isAlgoliaValid = config.appId && config.apiKey && config.indexName;
+        if (!isAlgoliaValid) {
+          return console.error("Algolia Search setting is invalid!");
         }
-      },
-    });
-  
-    const configure = instantsearch.widgets.configure({
-      hitsPerPage: searchConfig.hits.per_page ?? 5,
-    });
-  
-    const searchBox = instantsearch.widgets.searchBox({
-      container: "#algolia-search-input",
-      showReset: false,
-      showSubmit: false,
-      placeholder: searchConfig.languages.input_placeholder,
-      showLoadingIndicator: true,
-      searchOnEnterKeyPressOnly: true,
-      searchAsYouType: true,
-    });
-  
-    const algoliaHitItemTemplate = function (hit, { html, components }) {
-      const link = hit.url ? hit.url : GLOBAL_CONFIG.root + hit.path;
-      const result = hit._highlightResult;
-      return `
-        <a href="${link}" class="algolia-hit-item-link">
-        <span class="algolia-hits-item-title">${result.content.value || "no-title"}</span>
-        </a>`;
+        searchClient = algoliasearch(config.appId, config.apiKey);
+        indexName = config.indexName;
+      } else if (searchConfig.engine == 'meilisearch') {
+        const config = searchConfig.meilisearch;
+        const isAlgoliaValid = config.host && config.apiKey && config.indexName;
+        if (!isAlgoliaValid) {
+          return console.error("Algolia Search setting is invalid!");
+        }
+        searchClient = instantMeiliSearch(config.host, config.apiKey).searchClient;
+        indexName = config.indexName;
+      }
+    
+      const search = instantsearch({
+        indexName: indexName,
+        searchClient: searchClient,
+        searchFunction(helper) {
+          if (helper.state.query) {
+            let innerLoading = '<i class="icon-spinner spin"></i>';
+            document.getElementById("algolia-hits").innerHTML = innerLoading;
+            helper.search();
+          }
+        },
+      });
+    
+      const configure = instantsearch.widgets.configure({
+        hitsPerPage: searchConfig.hits.per_page ?? 5,
+      });
+    
+      const searchBox = instantsearch.widgets.searchBox({
+        container: "#algolia-search-input",
+        showReset: false,
+        showSubmit: false,
+        placeholder: searchConfig.languages.input_placeholder,
+        showLoadingIndicator: true,
+        searchOnEnterKeyPressOnly: true,
+        searchAsYouType: true,
+      });
+    
+      const algoliaHitItemTemplate = function (hit, { html, components }) {
+        const link = hit.url ? hit.url : GLOBAL_CONFIG.root + hit.path;
+        const result = hit._highlightResult;
+        return `
+          <a href="${link}" class="algolia-hit-item-link">
+          <span class="algolia-hits-item-title">${result.content.value || "no-title"}</span>
+          </a>`;
+      };
+
+      const meilisearchHitItemTemplate = function (hit, { html, components }) {
+        const link = hit.url ? hit.url : GLOBAL_CONFIG.root + hit.path;
+        const result = hit._highlightResult;
+        const category = hit.hierarchy_lvl0 ? result.hierarchy_lvl0.value : "";
+        const tag = hit.hierarchy_lvl1 ? result.hierarchy_lvl1.value : "";
+        const title = hit.hierarchy_lvl2 ? result.hierarchy_lvl2.value : "";
+        const description = hit.hierarchy_lvl3 ? result.hierarchy_lvl3.value : "";
+        const h1 = hit.hierarchy_lvl3 ? result.hierarchy_lvl3.value : "";
+        const h2 = hit.hierarchy_lvl4 ? result.hierarchy_lvl4.value : "";
+        const h3 = hit.hierarchy_lvl5 ? result.hierarchy_lvl5.value : "";
+        const content = hit.content ? result.content.value : "";
+        const l1 = title ? title : "";
+        const l3 = content ? content : "";
+        return `
+          <a href="${link}" class="algolia-hit-item-link">
+            <div class="algolia-hit-item-title">
+              <span style="font-size: x-small;">${category}</span>
+              ${title}
+              <span style="font-size: x-small;">${tag}</span>
+            </div>
+            <div class="" style="font-size: x-small;">
+              ${h1} ${h2} ${h3}
+            </div>
+            <div class="" style="font-size: small;">
+                <span class="algolia-hits-item-title">${description}${l3}</span>
+            </div>
+          </a>
+          `;
+      };
+
+      const hits = instantsearch.widgets.hits({
+        container: "#algolia-hits",
+        templates: {
+          item(hit, { html, components }) {
+            const loadingLogo = document.querySelector("#algolia-hits .icon-spinner");
+            if (loadingLogo) {
+              loadingLogo.style.display = "none";
+            }
+            setTimeout(() => {
+              document.querySelector("#algolia-search .ais-SearchBox-input").focus();
+            }, 200);
+            console.log(hit);
+            if (searchConfig.engine == 'meilisearch') {
+              return meilisearchHitItemTemplate(hit, { html, components });
+            }
+            else if (searchConfig.engine == 'algolia') {
+              return algoliaHitItemTemplate(hit, { html, components });
+            }
+          },
+          empty: function (data) {
+            const loadingLogo = document.querySelector("#algolia-hits .icon-spinner");
+            console.info(loadingLogo);
+            if (loadingLogo) {
+              loadingLogo.style.display = "none";
+            }
+            setTimeout(() => {
+              document.querySelector("#algolia-search .ais-SearchBox-input").focus();
+            }, 200);
+            return (
+              '<div id="algolia-hits-empty">' +
+              GLOBAL_CONFIG.search.languages.hits_empty.replace(/\$\{query}/, data.query) +
+              "</div>"
+            );
+          },
+        },
+        cssClasses: {
+          item: "algolia-hit-item",
+        },
+      });
+    
+      const stats = instantsearch.widgets.stats({
+        container: "#algolia-info > .algolia-stats",
+        templates: {
+          text: function (data) {
+            const stats = GLOBAL_CONFIG.search.languages.hits_stats
+              .replace(/\$\{hits}/, data.nbHits)
+              .replace(/\$\{time}/, data.processingTimeMS);
+            return `<hr>${stats}`;
+          },
+        },
+      });
+    
+      const pagination = instantsearch.widgets.pagination({
+        container: "#algolia-pagination",
+        totalPages: searchConfig.hits.per_page ?? 5,
+        templates: {
+          first: '<i class="icon-angle-double-left"></i>',
+          last: '<i class="icon-angle-double-right"></i>',
+          previous: '<i class="icon-angle-left"></i>',
+          next: '<i class="icon-angle-right"></i>',
+        },
+        scrollTo: false,
+        showFirstLast: false,
+        cssClasses: {
+          root: "pagination",
+          item: "pagination-item",
+          link: "page-number",
+          active: "current",
+          disabled: "disabled-item",
+        },
+      });
+    
+      search.addWidgets([configure, searchBox, hits, stats, pagination]);
+
+      search.start();
+
+      window.pjax && search.on("render", () => {
+        window.pjax.refresh(document.getElementById("algolia-hits"));
+      });   
     };
 
-    const meilisearchHitItemTemplate = function (hit, { html, components }) {
-      const link = hit.url ? hit.url : GLOBAL_CONFIG.root + hit.path;
-      const result = hit._highlightResult;
-      const category = hit.hierarchy_lvl0 ? result.hierarchy_lvl0.value : "";
-      const tag = hit.hierarchy_lvl1 ? result.hierarchy_lvl1.value : "";
-      const title = hit.hierarchy_lvl2 ? result.hierarchy_lvl2.value : "";
-      const description = hit.hierarchy_lvl3 ? result.hierarchy_lvl3.value : "";
-      const h1 = hit.hierarchy_lvl3 ? result.hierarchy_lvl3.value : "";
-      const h2 = hit.hierarchy_lvl4 ? result.hierarchy_lvl4.value : "";
-      const h3 = hit.hierarchy_lvl5 ? result.hierarchy_lvl5.value : "";
-      const content = hit.content ? result.content.value : "";
-      const l1 = title ? title : "";
-      const l3 = content ? content : "";
-      return `
-        <a href="${link}" class="algolia-hit-item-link">
-          <div class="algolia-hit-item-title">
-            <span style="font-size: x-small;">${category}</span>
-            ${title}
-            <span style="font-size: x-small;">${tag}</span>
-          </div>
-          <div class="" style="font-size: x-small;">
-            ${h1} ${h2} ${h3}
-          </div>
-          <div class="" style="font-size: small;">
-              <span class="algolia-hits-item-title">${description}${l3}</span>
-          </div>
-        </a>
-        `;
-    };
-
-    const hits = instantsearch.widgets.hits({
-      container: "#algolia-hits",
-      templates: {
-        item(hit, { html, components }) {
-          const loadingLogo = document.querySelector("#algolia-hits .icon-spinner");
-          if (loadingLogo) {
-            loadingLogo.style.display = "none";
-          }
-          setTimeout(() => {
-            document.querySelector("#algolia-search .ais-SearchBox-input").focus();
-          }, 200);
-          console.log(hit);
-          if (searchConfig.engine == 'meilisearch') {
-            return meilisearchHitItemTemplate(hit, { html, components });
-          }
-          else if (searchConfig.engine == 'algolia') {
-            return algoliaHitItemTemplate(hit, { html, components });
-          }
-        },
-        empty: function (data) {
-          const loadingLogo = document.querySelector("#algolia-hits .icon-spinner");
-          console.info(loadingLogo);
-          if (loadingLogo) {
-            loadingLogo.style.display = "none";
-          }
-          setTimeout(() => {
-            document.querySelector("#algolia-search .ais-SearchBox-input").focus();
-          }, 200);
-          return (
-            '<div id="algolia-hits-empty">' +
-            GLOBAL_CONFIG.search.languages.hits_empty.replace(/\$\{query}/, data.query) +
-            "</div>"
-          );
-        },
-      },
-      cssClasses: {
-        item: "algolia-hit-item",
-      },
-    });
-  
-    const stats = instantsearch.widgets.stats({
-      container: "#algolia-info > .algolia-stats",
-      templates: {
-        text: function (data) {
-          const stats = GLOBAL_CONFIG.search.languages.hits_stats
-            .replace(/\$\{hits}/, data.nbHits)
-            .replace(/\$\{time}/, data.processingTimeMS);
-          return `<hr>${stats}`;
-        },
-      },
-    });
-  
-    const pagination = instantsearch.widgets.pagination({
-      container: "#algolia-pagination",
-      totalPages: searchConfig.hits.per_page ?? 5,
-      templates: {
-        first: '<i class="icon-angle-double-left"></i>',
-        last: '<i class="icon-angle-double-right"></i>',
-        previous: '<i class="icon-angle-left"></i>',
-        next: '<i class="icon-angle-right"></i>',
-      },
-      scrollTo: false,
-      showFirstLast: false,
-      cssClasses: {
-        root: "pagination",
-        item: "pagination-item",
-        link: "page-number",
-        active: "current",
-        disabled: "disabled-item",
-      },
-    });
-  
-    search.addWidgets([configure, searchBox, hits, stats, pagination]);
-
-    search.start();
-  
     searchClickFn();
     searchFnOnce();
   
@@ -235,9 +248,4 @@ window.addEventListener("load", () => {
       !btf.isHidden($searchMask) && closeSearch();
       searchClickFn();
     });
-  
-    window.pjax &&
-      search.on("render", () => {
-        window.pjax.refresh(document.getElementById("algolia-hits"));
-      });
   });
