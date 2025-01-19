ea27c1183a09b8f2e09eafdf6bd1d81fef638ee4
Wang Jiaxiang <to@jiaxiang.wang>
2025-01-19 14:18:28 +0800

    ✨ Support meilisearch

--- static/js/search/algolia.js
+++ static/js/search/algolia.js
@@ -135,37 +135,63 @@ window.addEventListener("load", () => {
       };
 
       const meilisearchHitItemTemplate = function (hit, { html, components }) {
-        const link = hit.url ? hit.url : GLOBAL_CONFIG.root + hit.path;
-        const result = hit._highlightResult;
-        const category = hit.hierarchy_lvl0 ? result.hierarchy_lvl0.value : "";
-        const tag = hit.hierarchy_lvl1 ? result.hierarchy_lvl1.value : "";
-        const title = hit.hierarchy_lvl2 ? result.hierarchy_lvl2.value : "";
-        const description = hit.hierarchy_lvl3 ? result.hierarchy_lvl3.value : "";
-        const h1 = hit.hierarchy_lvl3 ? result.hierarchy_lvl3.value : "";
-        const h2 = hit.hierarchy_lvl4 ? result.hierarchy_lvl4.value : "";
-        const h3 = hit.hierarchy_lvl5 ? result.hierarchy_lvl5.value : "";
-        const content = hit.content ? result.content.value : "";
-        const l1 = title ? title : "";
-        const l3 = content ? content : "";
+        const group = hit;
+        const items = hit.items;
+
         return `
-          <a href="${link}" class="algolia-hit-item-link">
-            <div class="algolia-hit-item-title">
-              <span style="font-size: x-small;">${category}</span>
-              ${title}
-              <span style="font-size: x-small;">${tag}</span>
-            </div>
-            <div class="" style="font-size: x-small;">
-              ${h1} ${h2} ${h3}
-            </div>
-            <div class="" style="font-size: small;">
-                <span class="algolia-hits-item-title">${description}${l3}</span>
-            </div>
-          </a>
+          <div class="search-result-group">
+              <a href="${group.url}" class="algolia-hit-item-link search-result-title">
+                  ${group.title ? `<i class="icon-book-open"></i>` : ''}
+                  ${group.category ? `<span class="category">${group.category}</span> | ` : ''}
+                  <span>${group.title}</span>
+                  ${group.description ? `<span>${group.description}</span>` : ''}
+              </a>
+              <div class="group-items">
+                  ${items.map((item) => `
+                    <a href="${item.url}" class="algolia-hit-item-link">
+                      <div class="search-result-item">
+                        <div class="search-result-chapter">
+                          ${item.hierarchy_lvl3 ? `<span>${item._highlightResult.hierarchy_lvl3.value}</span>` : ''}
+                          ${item.hierarchy_lvl4 ? `<span> > ${item._highlightResult.hierarchy_lvl4.value}</span>` : ''}
+                          ${item.hierarchy_lvl5 ? `<span> > ${item._highlightResult.hierarchy_lvl5.value}</span>` : ''}
+                        </div>
+                        <div class="search-result-content">
+                          ${item.content ? `<span>${item._highlightResult.content.value}</span>` : ''}
+                        </div>
+                      </div>
+                    </a>
+                  `).join('')}
+              </div>
+          </div>
           `;
       };
 
       const hits = instantsearch.widgets.hits({
         container: "#algolia-hits",
+        transformItems: (items) => { // 对搜索结果进行分组
+          if (searchConfig.engine == 'algolia') {
+            return items;
+          }
+          const groupedItems = {};
+          items.forEach((item) => {
+              const groupKey = item.hierarchy_lvl2 || 'Uncategorized'; // 以文章标题作为分组键
+              if (!groupedItems[groupKey]) {
+                  groupedItems[groupKey] = {
+                      category: item.hierarchy_lvl0 ? item._highlightResult.hierarchy_lvl0.value : "",
+                      tag: item.hierarchy_lvl1 ? item._highlightResult.hierarchy_lvl1.value : "",
+                      title: item.hierarchy_lvl2 ? item._highlightResult.hierarchy_lvl2.value : "",
+                      url: item.url.split("#")[0], // 去除锚点
+                      // 如果描述没有高亮结果,则不在搜索结果展示
+                      description: item.hierarchy_lvl3 && item.hierarchy_lvl3 !== item._highlightResult.hierarchy_lvl3.value ? item._highlightResult.hierarchy_lvl3.value : "",
+                      items: [],
+                  };
+              }
+              groupedItems[groupKey].items.push(item);
+          });
+
+          // 将分组后的数据转换为数组
+          return Object.values(groupedItems);
+        },
         templates: {
           item(hit, { html, components }) {
             const loadingLogo = document.querySelector("#algolia-hits .icon-spinner");
